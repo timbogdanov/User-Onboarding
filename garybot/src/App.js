3d637f9f25e86447cc07e5bb937e1f2663365abe
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-17 13:29:04 -0700

    complete form with onInputChane, onCheckBoxChange, onSubmitChange handlers

--- garybot/src/App.js
+++ garybot/src/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './Form'
 
 function App() {
+
+  const initialFormValues = {
+    name: '',
+    email: '',
+    password: '',
+
+    terms: false,
+  }
+
+  const initialUsers = [
+    {
+      name: 'Murky',
+      email: 'murky@murk.com',
+      password: 'murky1999',
+    }
+  ]
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [users, setUsers] = useState(initialUsers)
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const onCheckBoxChange = (event) => {
+    const { name, checked } = event.target
+
+    setFormValues({
+      ...formValues,
+      [name]: checked
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form
+        onInputChange={onInputChange}
+        onCheckBoxChange={onCheckBoxChange}
+        onSubmit={onSubmit}
+        values={formValues}
+      />
     </div>
   );
 }
--- garybot/src/Form.js
+++ garybot/src/Form.js
@@ -1,34 +1,44 @@
 import React from 'react'
 
-const Form = () => {
+const Form = (props) => {
+
+  const { onInputChange, onCheckBoxChange, onSubmit, values } = props
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
 
       <input
         type='text'
         name='name'
         placeholder='Name'
+        onChange={onInputChange}
+        value={values.name}
       />
 
       <input
         type='text'
         name='email'
         placeholder='Email'
+        onChange={onInputChange}
+        value={values.email}
       />
 
       <input
         type='password'
         name='password'
         placeholder='Password'
+        onChange={onInputChange}
+        value={values.password}
       />
 
       <input
         type='checkbox'
         name='terms'
         id='terms'
+        onChange={onCheckBoxChange}
+        value={values.terms}
       />
-      <label for='terms'>Agree to Terms of Service</label>
+      <label htmlFor='terms'>Agree to Terms of Service</label>
 
       <input
         type='submit'
